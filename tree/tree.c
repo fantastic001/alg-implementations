2e84fa2de2e72408b712ea4e0c0d2d0ccb314981
Stefan Nožinić <stefan@lugons.org>
2017-01-05 16:30:10 +0100

    Implement fucking tree

---
+++ tree/tree.c
@@ -0,0 +1,89 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_st {
+	struct node_st *left; 
+	struct node_st *right; 
+	int val; 
+} node; 
+
+
+node* create_node(int a) 
+{
+	node* n = malloc(sizeof(int));
+	n->val = a; 
+	n->left = NULL; 
+	n->right = NULL;
+	return n; 
+}
+
+void insert(node* curr, int a) 
+{
+	if (a > curr->val) 
+	{
+		if (curr->right == NULL) curr->right = create_node(a);
+		else 
+		{
+			insert(curr->right, a);
+		}
+	}
+	if (a < curr->val) 
+	{
+		if (curr->left == NULL) curr->left = create_node(a);
+		else 
+		{
+			insert(curr->left, a);
+		}
+		
+	}
+}
+
+int intree(node* curr, int a) 
+{
+	if (a == curr->val) return 1; 
+	else 
+	{
+		if (a > curr->val) 
+		{
+			if (curr->right == NULL) return 0; 
+			else return intree(curr->right, a);
+		}
+		else 
+		{
+			if (curr->left == NULL) return 0; 
+			else return intree(curr->left, a);
+		}
+	}
+}
+
+void update_boundaries(node* curr, int depth, int* max, int* min) 
+{
+	if (curr->left == NULL && curr->right == NULL) 
+	{
+		if (*max < depth) *max = depth;
+		if (*min > depth) *min = depth;
+	}
+	else 
+	{
+		if (curr->left != NULL) update_boundaries(curr->left, depth+1, max, min);
+		if (curr->right != NULL) update_boundaries(curr->right, depth+1, max, min);
+	}
+}
+
+int main() {
+	node* root = create_node(0);
+	insert(root, 2);
+	insert(root, 1);
+	insert(root, 3);
+	insert(root, -1);
+	insert(root, -2);
+	insert(root, -3);
+	insert(root, -4);
+	insert(root, -5);
+	int max=0, min=1000000;
+	update_boundaries(root, 0, &max, &min);;
+	printf("max and min: %d %d\n", max, min);
+	return 0; 
+}
+
